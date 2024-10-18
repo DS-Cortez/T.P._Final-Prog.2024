20c8095c4f50d986831c0144cdf6697d05af0f20
camicauzzi <143765192+camicauzzi@users.noreply.github.com>
2024-10-17 21:13:26 -0300

    Update crear_examen.php

--- crear_examen.php
+++ crear_examen.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once 'ControlExamen.php';
 
@@ -9,29 +8,10 @@ $docente = $_POST['docente_examen'];
 $fecha = $_POST['fecha_examen'];
 
 
-$resultado = $control->crearExamen($materia, $nota, $docente, $fecha, $Materia_idMateria);
+$resultado = $control->crearExamen($materia, $nota, $docente, $fecha);
 
 if ($resultado) {
     echo "Examen creado exitosamente";
 } else {
     echo "Error al crear el examen";
 }
-=======
-<?php
-require_once 'ControlExamen.php';
-
-$control = new ControlExamen();
-$materia = $_POST['materia_examen'];
-$nota = $_POST['nota_examen'];
-$docente = $_POST['docente_examen'];
-$fecha = $_POST['fecha_examen'];
-
-
-$resultado = $control->crearExamen($materia, $nota, $docente, $fecha, $Materia_idMateria);
-
-if ($resultado) {
-    echo "Examen creado exitosamente";
-} else {
-    echo "Error al crear el examen";
-}
->>>>>>> bcaab2d1f066fbc0865572268d3a9f2c99268a19
