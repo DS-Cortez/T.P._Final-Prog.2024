e5aea8840f67eb5d6ec92aa5423932d17c2b0cdf
camicauzzi <143765192+camicauzzi@users.noreply.github.com>
2024-10-17 21:02:09 -0300

    Update ControlExamen.php

--- ControlExamen.php
+++ ControlExamen.php
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 <?php
 
 require_once 'RepoExamen.php';
 
 class ControlExamen
 {
-    public function crearExamen($materia, $nota, $docente, $fecha, $Materia_idMateria)
+    public function crearExamen($materia, $nota, $docente, $fecha)
     {
         $repo = new RepoExamen();
-        return $repo->crearExamen($materia, $nota, $docente, $fecha, $Materia_idMateria);
+        return $repo->crearExamen($materia, $nota, $docente, $fecha);
     }
 
     public function obtenerExamen($materia, $fecha)
@@ -23,41 +22,9 @@ class ControlExamen
         return $repo->eliminarExamen($materia, $fecha);
     }
 
-    public function modificarExamen($materia, $nota, $docente, $fecha, $Materia_idMateria)
+    public function modificarExamen($materia,$fecha,)
     {
         $repo = new RepoExamen();
-        return $repo->modificarExamen($materia, $nota, $docente, $fecha, $Materia_idMateria);
+        return $repo->modificarExamen($materia,$fecha);
     }
 }
-=======
-<?php
-
-require_once 'RepoExamen.php';
-
-class ControlExamen
-{
-    public function crearExamen($materia, $nota, $docente, $fecha, $Materia_idMateria)
-    {
-        $repo = new RepoExamen();
-        return $repo->crearExamen($materia, $nota, $docente, $fecha, $Materia_idMateria);
-    }
-
-    public function obtenerExamen($materia, $fecha)
-    {
-        $repo = new RepoExamen();
-        return $repo->obtenerExamen($materia, $fecha);
-    }
-
-    public function eliminarExamen($materia, $fecha)
-    {
-        $repo = new RepoExamen();
-        return $repo->eliminarExamen($materia, $fecha);
-    }
-
-    public function modificarExamen($materia, $nota, $docente, $fecha, $Materia_idMateria)
-    {
-        $repo = new RepoExamen();
-        return $repo->modificarExamen($materia, $nota, $docente, $fecha, $Materia_idMateria);
-    }
-}
->>>>>>> bcaab2d1f066fbc0865572268d3a9f2c99268a19
